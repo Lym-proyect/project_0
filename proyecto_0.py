af0caf6de18d75c104b520aad9ebb65aad1b7957
SamuelJt68 <s.ayalaa@uniandes.edu.co>
2023-08-31 16:48:20 -0500

    Cambios

--- proyecto_0.py
+++ proyecto_0.py
@@ -1,5 +1,38 @@
 
 check = False
+archivo = open("archivo.txt", "r")
 
-def comprobar():
-    archivo = None
+def remove(archivo: list):
+    
+    nuevo_archivo = []
+    
+    for caracter in archivo:
+        
+        if caracter == "\n":
+            archivo.remove(caracter)
+            
+        elif caracter == "\t":
+            archivo.remove(caracter)
+        
+        minuscula = caracter.lower()
+        nuevo_archivo.append(minuscula)
+    
+    return archivo
+
+def token(archivo):
+    archivo = remove(archivo)
+    tokens = []
+    for posicion in range(len(archivo)):
+        for caracter in archivo:
+            posicion += 1
+            if caracter == "jump" and posicion:
+                tokens.append("espacio")
+            
+        
+def verificar(tokens: list):
+    
+    if tokens.size() > 0:
+        check = True
+    
+    print(check)  
+    
