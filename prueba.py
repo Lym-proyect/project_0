9c6527fc41c0352f710c9b7fdf9c16bc80dae58e
Alexo31 <a.lancherosd@uniandes.edu.co>
2023-09-05 20:40:35 -0500

    update 8

--- prueba.py
+++ prueba.py
@@ -4,23 +4,74 @@ import codecs
 import sys
 import os
 
-tokens = ['JUMP', 'WALk','DEFVAR','DEFPROC']
+name = "ejemploFull.txt"
+
+tokens = ['JUMP', 'WALK','DEFVAR','DEFPROC','LEAP','TURN','DROP','TURNTO','GET','GRAB','LETGO','NOP','IF','FACING','CAN','NOT','OBRACKET']
 
 def t_JUMP(t):
 	r'jump\s*\(\s*(.*?)\s*\)'
 	return t
 
+def t_LEAP(t):
+	r'leap\s*\(\s*(.*?)\s*\)'
+	return t
+
+def t_TURN(t):
+	r'turn\s*\(\s*(.*?)\s*\)'
+	return t
+
 def t_WALK(t):
-	r'WALK'
+	r'walk\s*\(\s*(.*?)\s*\)'
+	return t
+
+def t_DROP(t):
+	r'drop\s*\(\s*(.*?)\s*\)'
 	return t
 
 def t_DEFVAR(t):
-    r'defvar\s+([a-zA-Z_][a-zA-Z]*)\s+([a-zA-Z0-9_]+)'
+    r'defvar\s+([a-zA-Z0-9_][a-zA-Z0-9_]*)\s+([a-zA-Z0-9_][a-zA-Z0-9_]*)'
     return t
 
 def t_DEFPROC(t):
-    r'defproc\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*\((.*?)\)\s*{([^}]*)}'    
+    r'defproc\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*\(\s*(.*?)\s*\)'    
     return t
+
+def t_TURNTO(t):
+	r'turnto\s*\(\s*(.*?)\s*\)'
+	return t
+def t_GET(t):
+	r'get\s*\(\s*(.*?)\s*\)'
+	return t
+def t_GRAB(t):
+	r'grab\s*\(\s*(.*?)\s*\)'
+	return t
+def t_LETGO(t):
+	r'letgo\s*\(\s*(.*?)\s*\)'
+	return t
+
+def t_NOP(t):
+	r'nop\s*\(\)'
+	return t
+
+def t_FACING(t):
+	r'facing\s*\(\s*(.*?)\s*\)'
+	return t
+
+def t_CAN(t):
+	r'can\s*\(\s*(.*?)\s*\)'
+	return t
+
+def t_NOT(t):
+	r'not'
+	return t
+
+def t_IF(t):
+	r'if+t_NOT|t_CAN|t_FACING+\('
+	return t
+
+def t_OBRACKET(t):
+	r'\{'
+	return t
 
 t_ignore = ' \t\n'
 
@@ -29,10 +80,10 @@ def t_error(t):
     t.lexer.skip(1)
 
 analizador_lexico = lex.lex()
+entrada = open(name, "r")
+cadena = entrada.read().lower()
 
-
-entrada = "jump (r,t)"
-analizador_lexico.input(entrada)
+analizador_lexico.input(cadena)
 
 while True:
     token = analizador_lexico.token()
@@ -40,5 +91,5 @@ while True:
         break  
     print(token)
 
-
+ 
 
