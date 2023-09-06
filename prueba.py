a236d9c55018384e9d6292abd17cd4fe3374c129
SamuelJt68 <s.ayalaa@uniandes.edu.co>
2023-09-05 21:35:38 -0500

    Full tokens

--- prueba.py
+++ prueba.py
@@ -4,9 +4,8 @@ import codecs
 import sys
 import os
 
-name = "ejemploFull.txt"
 
-tokens = ['JUMP', 'WALK','DEFVAR','DEFPROC','LEAP','TURN','DROP','TURNTO','GET','GRAB','LETGO','NOP','IF','FACING','CAN','NOT','OBRACKET']
+tokens = ['JUMP', 'WALK','DEFVAR','DEFPROC','LEAP','TURN','DROP','TURNTO','GET','GRAB','LETGO','NOP','IF','FACING','CAN','NOT','OBRACKET', "ELSE", "SEMICOLON", "ELIF", "WHILE", "REPEAT", "TIMES", "COMA", "EBRECKET"]
 
 def t_JUMP(t):
 	r'jump\s*\(\s*(.*?)\s*\)'
@@ -73,23 +72,69 @@ def t_OBRACKET(t):
 	r'\{'
 	return t
 
+def t_ELSE(t):
+    r"'\belse\b'"
+    return t
+
+def t_SEMICOLON(t):
+    r"'\;'"
+    return t
+
+def t_ELIF(t):
+    r"'\belif\b'"
+    return t    
+
+def t_WHILE(t):
+    r"'\bwhile\b'"
+    return t
+
+def t_REPEAT(t):
+    r"'\brepeat\b'"
+    return t
+
+def t_TIMES(t):
+    r"'\btimes\b'"
+    return t
+
+def t_COMA(t):
+    r"'\,'"
+    return t
+
+def t_EBRACKET(t):
+    r'\}'
+    return t 
+
 t_ignore = ' \t\n'
 
 def t_error(t):
     print(f"Caracter ilegal: {t.value[0]}")
     t.lexer.skip(1)
 
+
 analizador_lexico = lex.lex()
-entrada = open(name, "r")
-cadena = entrada.read().lower()
 
-analizador_lexico.input(cadena)
+working = True
 
-while True:
-    token = analizador_lexico.token()
-    if not token:
-        break  
-    print(token)
+while working:
+    print("\nBienvenido al analizador léxico de nuestro lenguaje de programación ")
+    print("\n1. Usar analizador léxico")
+    print("2. Salir")
+    opcion = input("\nIngrese una opción: ")
+    
+    if int(opcion) == 1:
+        archivo = input("Ingrese el nombre del archivo txt: ")
+        entrada = open(archivo, "r")
+        cadena = entrada.read().lower()
+        analizador_lexico.input(cadena)
+        while True:
+            token = analizador_lexico.token()
+            if not token:
+                break  
+            print(token)
+        
+    elif int(opcion) == 2:
+        working = False
+        print("\nGracias por utilizar el programa")
 
  
 
